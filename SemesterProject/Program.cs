dc44224b407909709de5e4ef8887c32df73b439c
Devon McCaffrey <McCaffreyDJ@cougars.sf.edu>
2025-02-17 19:10:18 -0500

    Programming Assignment 3

--- SemesterProject/Program.cs
+++ SemesterProject/Program.cs
@@ -4,7 +4,12 @@ public static class Program
 {
     public static void Main()
     {
-        var svg = new SvgBuilder(500, 500).Build();
+        var svg = new SvgBuilder(500, 500)
+
+        .AddRectangle(200, 100, 70, 300, "00FF00")
+        .AddRectangle(100, 150, 100, 200, "00FF00")
+        .AddRectangle(150, 200, 120, 100, "00FF00")
+        .Build();
 
         Console.Write("Absolute path to save SVG at: ");
         var path = Console.ReadLine() ?? "";
